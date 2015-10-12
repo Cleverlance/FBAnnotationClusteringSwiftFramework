99d1d31c2654113bc662e09ed840342d378bc7fa
Matej Zajic <matej.zajic@cleverlance.com>
2015-10-12 14:56:35 +0200

    Add Carthage

---
+++ Carthage/Build/iOS/FBAnnotationClusteringSwiftFramework.framework/Headers/FBAnnotationClusteringSwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  FBAnnotationClusteringSwiftFramework.h
+//  FBAnnotationClusteringSwiftFramework
+//
+//  Created by mzajic on 12.10.15.
+//  Copyright © 2015 mzajic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBAnnotationClusteringSwiftFramework.
+FOUNDATION_EXPORT double FBAnnotationClusteringSwiftFrameworkVersionNumber;
+
+//! Project version string for FBAnnotationClusteringSwiftFramework.
+FOUNDATION_EXPORT const unsigned char FBAnnotationClusteringSwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBAnnotationClusteringSwiftFramework/PublicHeader.h>
+
+
